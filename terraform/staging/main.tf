7592cb2c78391d61bd8be99e4a7d8d0ac1ae2428
Alex Demetriou <alex.demetriou@hackney.gov.uk>
2021-01-12 17:57:17 +0000

    fix sg

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -49,7 +49,7 @@ module "postgres_db_staging" {
   db_port  = 5829
   subnet_ids = data.aws_subnet_ids.staging.ids
   db_engine = "postgres"
-  db_engine_version = "11.8"
+  db_engine_version = "11.8" //DMS does not work well with v12
   db_instance_class = "db.t2.micro"
   db_allocated_storage = 20
   maintenance_window = "sun:10:00-sun:10:30"
