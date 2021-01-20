98668cb279defddaf6d70c2fe2a7ebff6d3bffee
Kyle Alexander <mcphylus@live.co.uk>
2021-01-20 16:51:51 +0000

    property reference migration

---
+++ RepairsApi/V2/Infrastructure/Migrations/20210120165114_PropRef.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepairsApi.V2.Infrastructure.Migrations
+{
+    public partial class PropRef : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "property_reference",
+                table: "property_class",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "property_reference",
+                table: "property_class");
+        }
+    }
+}
