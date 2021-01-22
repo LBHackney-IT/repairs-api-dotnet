0f23ebe833a6cfee94398ada9c196535eec08f40
Kyle Alexander <mcphylus@live.co.uk>
2021-01-22 12:35:16 +0000

    REmove duplicate properties

--- RepairsApi/V2/Infrastructure/Address.cs
+++ RepairsApi/V2/Infrastructure/Address.cs
@@ -33,19 +33,5 @@ namespace RepairsApi.V2.Infrastructure
     {
         [Key] public int Id { get; set; }
         public virtual PostalAddress Address { get; set; }
-        public string Postbox { get; set; }
-        public string Room { get; set; }
-        public string Department { get; set; }
-        public string Floor { get; set; }
-        public string Plot { get; set; }
-        public string BuildingNumber { get; set; }
-        public string BuildingName { get; set; }
-        public string ComplexName { get; set; }
-        public string StreetName { get; set; }
-        public string CityName { get; set; }
-        public CountryCode Country { get; set; }
-        public virtual List<string> AddressLine { get; set; }
-        public string Type { get; set; }
-        public string PostalCode { get; set; }
     }
 }
