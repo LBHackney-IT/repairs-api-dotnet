5b4448586e562ce6e22ad97da778e94b8db9d3bd
Kyle Alexander <mcphylus@live.co.uk>
2021-01-22 15:16:54 +0000

    Added generator helper extensions

--- RepairsApi.Tests/Helpers/StubGeneration/ObjectGenerationExtensions.cs
+++ RepairsApi.Tests/Helpers/StubGeneration/ObjectGenerationExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace RepairsApi.Tests.Helpers.StubGeneration
 {
@@ -12,13 +14,37 @@ namespace RepairsApi.Tests.Helpers.StubGeneration
                 .AddGenerator(new RandomBoolGenerator());
         }
 
+        public static Generator<T> AddGenerator<T, TValue>(this Generator<T> generator, TValue value)
+        {
+            return generator
+                .AddGenerator(new SimpleValueGenerator<TValue>(value));
+        }
+
+        public static Generator<T> AddGenerator<T, TValue>(this Generator<T> generator, Func<TValue> valueGenerator)
+        {
+            return generator
+                .AddGenerator(new SimpleValueGenerator<TValue>(valueGenerator));
+        }
+
+        public static Generator<T> AddGenerator<T, TModel, TProp>(this Generator<T> generator, TProp value, params Expression<Func<TModel, TProp>>[] accessors)
+        {
+            return generator
+                .AddGenerator(new SimpleValueGenerator<TProp>(value), accessors);
+        }
+
+        public static Generator<T> AddGenerator<T, TModel, TProp>(this Generator<T> generator, Func<TProp> valueGenerator, params Expression<Func<TModel, TProp>>[] accessors)
+        {
+            return generator
+                .AddGenerator(new SimpleValueGenerator<TProp>(valueGenerator), accessors);
+        }
+
         public static Generator<T> AddWorkOrderGenerators<T>(this Generator<T> generator)
         {
             return generator
                 .AddDefaultGenerators()
-                .AddGenerator(new SimpleValueGenerator<ICollection<string>>(new string[] { "hello", "me" }), (RepairsApi.V2.Generated.PropertyAddress addr) => addr.AddressLine)
-                .AddGenerator(new SimpleValueGenerator<ICollection<double>>(new double[] { 2.0 }), (RepairsApi.V2.Generated.Quantity q) => q.Amount)
-                .AddGenerator(new SimpleValueGenerator<ICollection<string>>(new string[] { "2.0" }),
+                .AddGenerator(new string[] { "address", "line" }, (RepairsApi.V2.Generated.PropertyAddress addr) => addr.AddressLine)
+                .AddGenerator(new double[] { 2.0 }, (RepairsApi.V2.Generated.Quantity q) => q.Amount)
+                .AddGenerator(new string[] { "2.0" },
                     (RepairsApi.V2.Generated.GeographicalLocation q) => q.Latitude,
                     (RepairsApi.V2.Generated.GeographicalLocation q) => q.Longitude,
                     (RepairsApi.V2.Generated.GeographicalLocation q) => q.Elevation,
