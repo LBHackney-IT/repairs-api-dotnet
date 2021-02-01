29abd15a51f31a1083c34aeee4243fdaa0ef4925
Kyle Alexander <mcphylus@live.co.uk>
2021-02-01 11:25:31 +0000

    SOR models

--- RepairsApi/V2/Infrastructure/ScheduleOfRates.cs
+++ RepairsApi/V2/Infrastructure/ScheduleOfRates.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace RepairsApi.V2.Infrastructure
@@ -29,12 +30,16 @@ namespace RepairsApi.V2.Infrastructure
         [Key] public string TradeCode { get; set; }
 
         public string Description { get; set; }
+
+        public virtual List<SORContractor> Contractors { get; set; }
     }
 
     public class SORContractor
     {
         [Key] public string SORContractorRef { get; set; }
         public string Name { get; set; }
+
+        public virtual List<SORContract> Contracts { get; set; }
     }
 
     public class SORContract
@@ -43,5 +48,8 @@ namespace RepairsApi.V2.Infrastructure
 
         public int PropertyConstraintMethodId { get; set; }
         public string PropertyConstraintParameters { get; set; }
+
+        public virtual SORContractor Contractor { get; set; }
+        public virtual List<SORTrade> Contractors { get; set; }
     }
 }
