be106dd8112607bc5a9be499e55be236221c1fb0
Kyle Alexander <mcphylus@live.co.uk>
2021-02-05 11:30:18 +0000

    use consts for tests

--- RepairsApi.Tests/V2/Services/CurrentUserServiceTests.cs
+++ RepairsApi.Tests/V2/Services/CurrentUserServiceTests.cs
@@ -18,14 +18,14 @@ namespace RepairsApi.Tests.V2.Services
         [Test]
         public void SetsUser()
         {
-            const string JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMDA1MTg4ODg3NDY5MjIxMTY2NDciLCJlbWFpbCI6ImhhY2tuZXkudXNlckB0ZXN0LmhhY2tuZXkuZ292LnVrIiwiaXNzIjoiSGFja25leSIsIm5hbWUiOiJIYWNrbmV5IFVzZXIiLCJncm91cHMiOlsiZ3JvdXAgMSIsImdyb3VwIDIiXSwiaWF0IjoxNTcwNDYyNzMyfQ.BxBlEHWHGU6GkPO5DZoshJp3VQcrm2umaMkQ7Q7zxw8";
+            const string JWT = TestUserInformation.JWT;
             _classUnderTest.LoadUser(JWT);
 
             var user = _classUnderTest.GetUser();
 
             user.Should().NotBeNull();
-            user.Name.Should().Be("Hackney User");
-            user.Email.Should().Be("hackney.user@test.hackney.gov.uk");
+            user.Name.Should().Be(TestUserInformation.NAME);
+            user.Email.Should().Be(TestUserInformation.EMAIL);
         }
 
         [Test]
