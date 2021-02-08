f1d779d8865e223ff987cbe10e5d74b20a12879d
Kyle Alexander <mcphylus@live.co.uk>
2021-02-08 15:33:31 +0000

    test fixes

--- RepairsApi.Tests/V2/UseCase/ListAppointmentsUseCaseTests.cs
+++ RepairsApi.Tests/V2/UseCase/ListAppointmentsUseCaseTests.cs
@@ -33,15 +33,5 @@ namespace RepairsApi.Tests.V2.UseCase
 
             await testFn.Should().ThrowAsync<ResourceNotFoundException>();
         }
-
-        [Test]
-        public async Task ThrowsIfNoWorkOrder()
-        {
-            _repairGatewayMock.Setup(rgm => rgm.GetWorkOrder(It.IsAny<int>())).ReturnsAsync((WorkOrder) null);
-            int workOrder = 0;
-            Func<Task> testFn = async () => await _classUnderTest.Execute(workOrder, DateTime.UtcNow, DateTime.UtcNow);
-
-            await testFn.Should().ThrowAsync<ResourceNotFoundException>();
-        }
     }
 }
