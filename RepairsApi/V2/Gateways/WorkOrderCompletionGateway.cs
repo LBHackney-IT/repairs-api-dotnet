cfcbac5d0f66876a5da22927ec4c893dd93fe310
Joshua Dadswell <josh.dadswell@hackney.gov.uk>
2021-02-17 15:35:59 +0000

    Add author to notes raised through complete

--- RepairsApi/V2/Gateways/WorkOrderCompletionGateway.cs
+++ RepairsApi/V2/Gateways/WorkOrderCompletionGateway.cs
@@ -2,16 +2,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RepairsApi.V2.Infrastructure;
+using RepairsApi.V2.Services;
 
 namespace RepairsApi.V2.Gateways
 {
     public class WorkOrderCompletionGateway : IWorkOrderCompletionGateway
     {
         private readonly RepairsContext _repairsContext;
+        private readonly ICurrentUserService _currentUserService;
 
-        public WorkOrderCompletionGateway(RepairsContext repairsContext)
+        public WorkOrderCompletionGateway(RepairsContext repairsContext, ICurrentUserService currentUserService)
         {
             _repairsContext = repairsContext;
+            _currentUserService = currentUserService;
         }
 
         public async Task<bool> IsWorkOrderCompleted(int id)
@@ -21,6 +24,13 @@ namespace RepairsApi.V2.Gateways
 
         public async Task<int> CreateWorkOrderCompletion(WorkOrderComplete completion)
         {
+            var user = _currentUserService.GetUser();
+            completion.JobStatusUpdates.ForEach(jsu =>
+            {
+                jsu.AuthorName = user.Name;
+                jsu.AuthorEmail = user.Email;
+            });
+
             _repairsContext.WorkOrderCompletes.Add(completion);
             await _repairsContext.SaveChangesAsync();
 
