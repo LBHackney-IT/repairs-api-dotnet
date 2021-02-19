3cc73ccae034ee0010dde3b3a8792496491585c9
Joshua Dadswell <josh.dadswell@hackney.gov.uk>
2021-02-19 10:42:51 +0000

    migration

---
+++ RepairsApi/V2/Infrastructure/Migrations/20210219104002_OriginalFlag.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepairsApi.V2.Infrastructure.Migrations
+{
+    public partial class OriginalFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "original",
+                table: "rate_schedule_item",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "original",
+                table: "rate_schedule_item");
+        }
+    }
+}
