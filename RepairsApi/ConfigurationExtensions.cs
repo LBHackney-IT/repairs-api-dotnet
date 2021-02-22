4bf4a8417940a785090b332bfb8319a9bed2d07c
Kyle Alexander <mcphylus@live.co.uk>
2021-02-22 14:00:29 +0000

    pull from environment

---
+++ RepairsApi/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+
+namespace RepairsApi
+{
+    public static class ConfigurationExtensions
+    {
+        [SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Handled by builder")]
+        public static void AddGroups(this IConfigurationBuilder builder)
+        {
+            var json = Environment.GetEnvironmentVariable("GROUPS");
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return;
+            }
+
+            builder.AddJsonStream(new MemoryStream(Encoding.ASCII.GetBytes(json)));
+        }
+    }
+}
