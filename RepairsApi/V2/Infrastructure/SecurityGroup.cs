d768b0ecd83b3b76e6aaec923e07e1b4e3361554
Kyle Alexander <mcphylus@live.co.uk>
2021-02-23 15:50:55 +0000

    formatting

--- RepairsApi/V2/Infrastructure/SecurityGroup.cs
+++ RepairsApi/V2/Infrastructure/SecurityGroup.cs
@@ -4,7 +4,7 @@ namespace RepairsApi.V2.Infrastructure
 {
     public class SecurityGroup
     {
-        [Key]public string GroupName { get; set; }
+        [Key] public string GroupName { get; set; }
         public string UserType { get; set; }
         public string ContractorReference { get; set; }
         public double? RaiseLimit { get; set; }
