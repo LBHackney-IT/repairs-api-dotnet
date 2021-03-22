957c8e50e8ac0b025ef8068064a403426c4e633e
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2021-03-22 16:20:16 +0000

    Change has variations column to boolen

---
+++ RepairsApi/V2/Infrastructure/Migrations/20210322161828_AddVariationsColumnDefaultValue.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepairsApi.V2.Infrastructure.Migrations
+{
+    public partial class AddVariationsColumnDefaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- RepairsApi/V2/Infrastructure/WorkOrder.cs
+++ RepairsApi/V2/Infrastructure/WorkOrder.cs
@@ -34,7 +34,7 @@ namespace RepairsApi.V2.Infrastructure
         // Extensions
         public string AgentName { get; set; }
         public WorkStatusCode StatusCode { get; set; } = WorkStatusCode.Open;
-        public VariationStatusCode HasVariation { get; set; } = VariationStatusCode.Outstanding;
+        public bool HasVariation { get; set; } = false;
     }
 
     public enum WorkStatusCode
@@ -55,12 +55,6 @@ namespace RepairsApi.V2.Infrastructure
 
         // Extensions
         NoAccess = 1000,
-    }
-
-    public enum VariationStatusCode
-    {
-        Resolved = 0,
-        Outstanding = 1
     }
 
     [Owned]
