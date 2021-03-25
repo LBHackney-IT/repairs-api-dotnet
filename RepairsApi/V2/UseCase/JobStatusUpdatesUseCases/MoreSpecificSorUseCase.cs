b51e8ab3e3f4f6057ebaa0ec7272a3cc10304072
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2021-03-25 15:59:27 +0000

    add enum reason code column

--- RepairsApi/V2/UseCase/JobStatusUpdatesUseCases/MoreSpecificSorUseCase.cs
+++ RepairsApi/V2/UseCase/JobStatusUpdatesUseCases/MoreSpecificSorUseCase.cs
@@ -42,7 +42,7 @@ namespace RepairsApi.V2.UseCase.JobStatusUpdatesUseCases
 
             var authorised = await _authorizationService.AuthorizeAsync(_currentUserService.GetUser(), jobStatusUpdate, "VarySpendLimit");
             if (await _featureManager.IsEnabledAsync(FeatureFlags.SPENDLIMITS) && !authorised.Succeeded)
-                workOrder.StatusCode = WorkStatusCode.PendApp;
+                workOrder.StatusCode = WorkStatusCode.PendApp; //change to on hold - reason code will be 60 (no approval)
 
             var existingCodes = workOrder.WorkElements.SelectMany(we => we.RateScheduleItem);
             var newCodes = workElement.RateScheduleItem.Where(rsi => !existingCodes.Any(ec => ec.Id == rsi.Id));
