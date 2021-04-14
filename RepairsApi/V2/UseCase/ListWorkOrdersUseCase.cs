8a4fafc51657e939bc42f534127b8e7274079c1b
Kyle Alexander <kyle.alexander@ijyi.com>
2021-04-14 15:15:38 +0100

    Filter POC

--- RepairsApi/V2/UseCase/ListWorkOrdersUseCase.cs
+++ RepairsApi/V2/UseCase/ListWorkOrdersUseCase.cs
@@ -61,7 +61,103 @@ namespace RepairsApi.V2.UseCase
                 result.Add(wo => wo.StatusCode == (WorkStatusCode) Enum.Parse(typeof(WorkStatusCode), searchParameters.StatusCode.ToString()));
             }
 
+            var filterBuilder = Test();
+
+            var filter = filterBuilder.BuildFilter(searchParameters);
+
             return result.ToArray();
         }
+
+        public static FilterBuilder<WorkOrderSearchParameters, WorkOrder> Test()
+        {
+            return new FilterBuilder<WorkOrderSearchParameters, WorkOrder>()
+                .AddFilter(
+                    searchParams => searchParams.ContractorReference,
+                    contractorReference => !string.IsNullOrWhiteSpace(contractorReference),
+                    contractorReference => wo => wo.AssignedToPrimary.ContractorReference == contractorReference
+                )
+                .AddFilter(
+                    searchParams => searchParams.PropertyReference,
+                    p => !string.IsNullOrWhiteSpace(p),
+                    p => wo => wo.Site.PropertyClass.Any(pc => pc.PropertyReference == p)
+                )
+                .AddFilter(
+                    searchParams => searchParams.StatusCode,
+                    code => code > 0 && Enum.IsDefined(typeof(WorkStatusCode), code),
+                    code => wo => wo.StatusCode == (WorkStatusCode)Enum.Parse(typeof(WorkStatusCode), code.ToString())
+                );
+        }
+    }
+
+    public class FilterBuilder<TSearch, TQuery>
+    {
+        private List<IFilterItem<TSearch, TQuery>> _config = new List<IFilterItem<TSearch, TQuery>>();
+
+        internal FilterBuilder<TSearch, TQuery> AddFilter<T>(Func<TSearch, T> searchValueFunction, Func<T, bool> searchValidator, Func<T, Expression<Func<TQuery, bool>>> filterFunction)
+        {
+           _config.Add(new FilterItem<T, TSearch, TQuery>(searchValueFunction, searchValidator, filterFunction));
+           return this;
+        }
+
+        internal IFilter<TQuery> BuildFilter(TSearch searchParameter)
+        {
+            return new Filter<TQuery>(_config.Where(c => c.IsValid(searchParameter)).Select(c => c.CreateExpression(searchParameter)));
+        }
+    }
+
+    public class Filter<TQuery> : IFilter<TQuery>
+    {
+        private IEnumerable<Expression<Func<TQuery, bool>>> _predicates;
+
+        public Filter(IEnumerable<Expression<Func<TQuery, bool>>> predicates)
+        {
+            _predicates = predicates;
+        }
+
+        public IQueryable<TQuery> Apply(IQueryable<TQuery> query)
+        {
+            return _predicates.Aggregate(query, (q, pred) => q.Where(pred));
+        }
+    }
+
+    public interface IFilter<TQuery>
+    {
+        IQueryable<TQuery> Apply(IQueryable<TQuery> query);
+    }
+
+    public class FilterItem<T, TSearch, TQuery> : IFilterItem<TSearch, TQuery>
+    {
+        private Func<TSearch, T> _searchValueFunction;
+        private readonly Func<T, bool> _searchValidator;
+        private Func<T, Expression<Func<TQuery, bool>>> _filterFunction;
+
+        public FilterItem(Func<TSearch, T> p, Func<T, bool> searchValidator, Func<T, Expression<Func<TQuery, bool>>> predicate)
+        {
+            this._searchValueFunction = p;
+            _searchValidator = searchValidator;
+            this._filterFunction = predicate;
+        }
+
+        public Expression<Func<TQuery, bool>> CreateExpression(TSearch search)
+        {
+            T value = _searchValueFunction(search);
+
+            var filterExpression = _filterFunction(value);
+
+            return filterExpression;
+        }
+
+        public bool IsValid(TSearch search)
+        {
+            T value = _searchValueFunction(search);
+            return _searchValidator(value);
+        }
+    }
+
+    public interface IFilterItem<TSearch, TQuery>
+    {
+        Expression<Func<TQuery, bool>> CreateExpression(TSearch search);
+
+        bool IsValid(TSearch search);
     }
 }
