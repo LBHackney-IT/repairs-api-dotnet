bc576c4b60ec47fece3e727716a25980069d1023
Kyle Alexander <kyle.alexander@ijyi.com>
2021-04-28 11:53:44 +0100

    mock helpers

--- RepairsApi.Tests/V2/UseCase/NotificationMock.cs
+++ RepairsApi.Tests/V2/UseCase/NotificationMock.cs
@@ -2,19 +2,28 @@ using RepairsApi.V2.Notifications;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RepairsApi.Tests.V2.UseCase
 {
     internal class NotificationMock : INotifier
     {
-        private bool _hasBeenCalled = false;
+        private readonly List<Type> _calledTypes = new List<Type>();
+        private readonly List<object> _notifications = new List<object>();
 
-        public bool HaveHandlersBeenCalled() => _hasBeenCalled;
+        public bool HaveHandlersBeenCalled<T>() => _calledTypes.Contains(typeof(T));
+
+        public List<T> GetNotifications<T>() => _notifications.OfType<T>().ToList();
+
+        public T GetLastNotification<T>() => _notifications.OfType<T>().LastOrDefault();
+
+        public List<object> GetNotifications() => _notifications;
 
         public Task Notify<T>(T notification) where T : INotification
         {
-            _hasBeenCalled = true;
+            _calledTypes.Add(typeof(T));
+            _notifications.Add(notification);
             return Task.CompletedTask;
         }
     }
