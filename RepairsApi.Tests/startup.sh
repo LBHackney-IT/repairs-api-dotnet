fe671751c5f2e7c2471e27dcf43a74d9d5de1d3f
Kyle Alexander <kyle.alexander@ijyi.com>
2021-05-10 14:13:08 +0100

    Aggregate Errors

--- RepairsApi.Tests/startup.sh
+++ RepairsApi.Tests/startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-dotnet test /p:CollectCoverage=true /p:Threshold=90 /p:ThresholdType=line /p:ExcludeByFile="**/Migrations/**/*.*%2c**/Generated/**/*.cs" --no-build --filter FullyQualifiedName~V2
-dotnet test --filter FullyQualifiedName~E2ETests
+failed=0
+dotnet test /p:CollectCoverage=true /p:Threshold=90 /p:ThresholdType=line /p:ExcludeByFile="**/Migrations/**/*.*%2c**/Generated/**/*.cs" --no-build --filter FullyQualifiedName~V2 || failed=1
+dotnet test --filter FullyQualifiedName~E2ETests || failed=1
+
+echo $failed
+exit $failed
