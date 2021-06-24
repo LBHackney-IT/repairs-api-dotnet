ed2c2b29b67cf71b48c4d326a86d8eec517c34d8
Joshua Dadswell <josh.dadswell@hackney.gov.uk>
2021-06-24 14:35:12 +0100

    convert times correctly

---
+++ RepairsApi/V2/Services/DRS/DrsHelpers.cs
@@ -0,0 +1,22 @@
+using System;
+using NodaTime;
+
+namespace RepairsApi.V2.Services.DRS
+{
+    public static class DrsHelpers
+    {
+        public static DateTime ConvertToDrsTimeZone(DateTime dateTime)
+        {
+            var london = DateTimeZoneProviders.Tzdb["Europe/London"];
+            var utcDateTime = new DateTime(dateTime.Ticks, DateTimeKind.Utc);
+            var local = Instant.FromDateTimeUtc(utcDateTime).InUtc();
+            return local.WithZone(london).ToDateTimeUnspecified();
+        }
+
+        public static DateTime ConvertFromDrsTimeZone(DateTime dateTime)
+        {
+            var london = DateTimeZoneProviders.Tzdb["Europe/London"];
+            return dateTime - london.GetUtcOffset(Instant.FromDateTimeUtc(dateTime)).ToTimeSpan();
+        }
+    }
+}
