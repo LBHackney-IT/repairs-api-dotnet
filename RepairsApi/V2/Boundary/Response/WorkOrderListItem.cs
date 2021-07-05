7cb20c81ad809cad127329869c2ce95afd0f5a34
Dave Dean <david.dean@hackney.gov.uk>
2021-07-05 13:33:52 +0100

    Adding locked status for migrated work orders

--- RepairsApi/V2/Boundary/Response/WorkOrderListItem.cs
+++ RepairsApi/V2/Boundary/Response/WorkOrderListItem.cs
@@ -14,6 +14,7 @@ namespace RepairsApi.V2.Boundary.Response
         public const string VariationApproved = "Variation Approved";
         public const string VariationRejected = "Variation Rejected";
         public const string NoAccess = "No Access";
+        public const string Locked = "Locked";
         public const string Unknown = "Unknown";
     }
 
