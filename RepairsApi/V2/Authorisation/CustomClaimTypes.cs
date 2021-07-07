cce793b03a21e5391dbc743891297b41dae941f8
Kyle Alexander <kyle.alexander@ijyi.com>
2021-07-07 12:15:54 +0100

    Added feature filter based on user google groups

--- RepairsApi/V2/Authorisation/CustomClaimTypes.cs
+++ RepairsApi/V2/Authorisation/CustomClaimTypes.cs
@@ -3,6 +3,7 @@ namespace RepairsApi.V2.Authorisation
     public static class CustomClaimTypes
     {
         public const string Contractor = "contractor";
+        public const string Group = "group";
         public const string RaiseLimit = "raiselimit";
         public const string VaryLimit = "varylimit";
     }
